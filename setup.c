7594e51b7c31edbaeb7da87c042d264859705637
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-03 18:33:27 +0100

    read_early_config(): really discover .git/
    Earlier, we punted and simply assumed that we are in the top-level
    directory of the project, and that there is no .git file but a .git/
    directory so that we can read directly from .git/config.
    
    However, that is not necessarily true. We may be in a subdirectory. Or
    .git may be a gitfile. Or the environment variable GIT_DIR may be set.
    
    To remedy this situation, we just refactored the way
    setup_git_directory() discovers the .git/ directory, to make it
    reusable, and more importantly, to leave all global variables and the
    current working directory alone.
    
    Let's discover the .git/ directory correctly in read_early_config() by
    using that new function.
    
    This fixes 4 known breakages in t7006.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -926,8 +926,9 @@ static enum discovery_result setup_git_directory_gently_1(struct strbuf *dir,
 
 const char *discover_git_directory(struct strbuf *gitdir)
 {
-	struct strbuf dir = STRBUF_INIT;
+	struct strbuf dir = STRBUF_INIT, err = STRBUF_INIT;
 	size_t gitdir_offset = gitdir->len, cwd_len;
+	struct repository_format candidate;
 
 	if (strbuf_getcwd(&dir))
 		return NULL;
@@ -950,7 +951,18 @@ const char *discover_git_directory(struct strbuf *gitdir)
 			strbuf_addch(&dir, '/');
 		strbuf_insert(gitdir, gitdir_offset, dir.buf, dir.len);
 	}
+
+	strbuf_reset(&dir);
+	strbuf_addf(&dir, "%s/config", gitdir->buf + gitdir_offset);
+	read_repository_format(&candidate, dir.buf);
 	strbuf_release(&dir);
+
+	if (verify_repository_format(&candidate, &err) < 0) {
+		warning("ignoring git dir '%s': %s",
+			gitdir->buf + gitdir_offset, err.buf);
+		strbuf_release(&err);
+		return NULL;
+	}
 
 	return gitdir->buf;
 }
